7acc76a99adfac7a673243c8775d975b32535d59
Morin Bruno <morinbruno@hotmail.be>
2025-01-09 19:41:19 +0100

    Connection à MongoDB avec Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule, 
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory:async (configService: ConfigService) => {
+        const user = configService.get('MONGO_DB_USER');
+        const password = configService.get('MONGO_DB_PASSWORD');
+        const host = configService.get('MONGO_DB_HOST');
+        const port = configService.get('MONGO_DB_PORT');
+        const dbName = configService.get('MONGO_DB_NAME');
+
+        return {
+          uri: `mongodb://${user}:${password}@${host}:${port}/${dbName}`,
+          authSource: "admin"
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  private logger = new Logger(AppService.name);
+
+  constructor(private configService: ConfigService) {
+   const mongoDbHost = configService.get('MONGO_DB_HOST');
+   this.logger.debug('MongoDB Host: ' + mongoDbHost);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
